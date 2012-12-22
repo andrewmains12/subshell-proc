65bc534f8705011c69816f93bfc34cf00d43610f
Andrew Mains <andrewmains12@berkeley.edu>
2012-12-22 03:06:07 -0800

    Added package metadata

--- subshell-proc.el
+++ subshell-proc.el
@@ -1,6 +1,20 @@
 ;;; subshell-proc.el --- Functions for working with comints
 ;; Author: Andrew Mains
-;; URL: 
+;; URL: https://github.com/andrewmains12/subshell-proc
+;; Version: 1.0.0
+;;
+;; Sample usage:
+;;
+;; ;;In elisp code
+;; (defproc "noir-server" "lein" '("run")
+;; "Run a noir server")  
+;;
+;; While editing:
+;;   M-x run-proc 
+;;     bash
+;;   will bring up a comint running bash  
+;;
+;;
 
 (require 'cl)
 (provide 'subshell-proc)
@@ -8,16 +22,16 @@
 
 (defmacro defproc (fn-name command command-args &optional docstring)
   "Defines an interactive function which creates a comint subprocess using command"
-  (let* ((fn-sym (intern fn-name))
-        )
+  (let* ((fn-sym (intern fn-name)))
+
     `(defun ,fn-sym (&rest extra-args)       
        ,docstring
        (interactive)
        (funcall 
         (make-proc-run-fn ,command ,command-args ,(format "*%s*" fn-name)))
-     )
-  )
-)
+     )))
+
+
 (defun make-proc-run-fn (command command-args &optional buffer-name)
   (lexical-let ((buffer-name buffer-name)
                 (command command)
@@ -30,18 +44,15 @@
                   (pop-to-buffer buffer)
                   (apply 'make-comint-in-buffer 
                          (append (list buffer-name buffer command nil) command-args))
-                  )
-                )
-              )
-    )
-  )
- 
+                  )))))
+
+
 (defun run-proc (command &optional buffer-name)
   (interactive "MCommand to run: ")
   (let ((command-list (split-string command)))
-    (funcall (make-proc-run-fn (car command-list) (cdr command-list) buffer-name))
-    )
-  )
+    (funcall (make-proc-run-fn (car command-list) (cdr command-list) buffer-name))))
+
+
 
 
 ;;; subshell-proc.el ends here
