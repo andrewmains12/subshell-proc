7f3d925de5269199b0c3183a45c371c01e9ea7bc
Andrew Mains <amains@uber.com>
2018-02-23 08:46:19 -0800

    Add license

--- subshell-proc.el
+++ subshell-proc.el
@@ -3,16 +3,33 @@
 ;; URL: https://github.com/andrewmains12/subshell-proc
 ;; Version: 0.2
 ;;
+;; License: GPL-3+
+;;
+;; Copyright (c) 2012 Andrew Mains
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 ;; Sample usage:
 ;;
 ;; ;;In elisp code
 ;; (defproc noir-server "lein" '("run")
-;; "Run a noir server")  
+;; "Run a noir server")
 ;;
 ;; While editing:
-;;   M-x run-proc 
+;;   M-x run-proc
 ;;     bash
-;;   will bring up a comint running bash  
+;;   will bring up a comint running bash
 ;;
 ;;
 
@@ -21,11 +38,11 @@
 
 (defmacro defproc (fn-name command command-args &optional docstring)
   "Defines an interactive function which creates a comint subprocess using command"
-    `(defun ,fn-name (&rest extra-args)       
+    `(defun ,fn-name (&rest extra-args)
        ,docstring
        (interactive)
-       (funcall 
-        (make-proc-run-fn ,command ,command-args 
+       (funcall
+        (make-proc-run-fn ,command ,command-args
                           ,(format "*%s*" (symbol-name fn-name))))
      ))
 
@@ -35,12 +52,12 @@
                 (command command)
                 (command-args command-args)
                 )
-    (function (lambda (&rest extra-args)                
+    (function (lambda (&rest extra-args)
                 (let* ((buffer-name (or buffer-name (format "*%s*" command)))
                        (buffer (get-buffer-create buffer-name))
                        )
                   (pop-to-buffer buffer)
-                  (apply 'make-comint-in-buffer 
+                  (apply 'make-comint-in-buffer
                          (append (list buffer-name buffer command nil) command-args))
                   )))))
 
